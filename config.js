6f53db486115b6ec21257c0120e92f5da88c30ac
lindingfeng <1758765238@qq.com>
2020-10-06 14:54:44 +0800

    master update 1006140000

--- config.js
+++ config.js
@@ -4,14 +4,53 @@ const chalk = require('chalk')
 const config = {
   AMC_LOGIN_HOST: 'http://api-amc.huishoubao.com.cn/login',
   AMC_API_HOST: 'api-amc.huishoubao.com.cn',
-  VERSION: '3.0.0',
+  VERSION_2: '2.0.0',
+  VERSION_3: '3.0.0',
   MSG_TYPE: 'request',
   REMARK: '',
   TIMESTAMPS: '',
+  DEFAULT_ZY_FTID: '21923671',
+  COMMAND_LINE: 5,
   BASE_LIST: [
     { name: '提交发版工单 (devops-prod)', value: 1 },
     { name: '新建发布任务 (devops-new)', value: 2 },
-    { name: '上传资源资源 (production)', value: 3 }
+    { name: '上传静态资源 (production)', value: 3 },
+    { name: '压缩静态资源 (zip、tar)', value: 4 },
+  ],
+  VERSION_SCOPE: [
+    { name: '大版本迭代', value: 0 },
+    { name: '小功能迭代', value: 1 },
+    { name: '优化迭代', value: 2 }
+  ],
+  COMPRESS_TEMPLATE_LIST: [
+    {
+      name: 'type',
+      message: '请选择压缩格式',
+      type: 'list',
+      choices: ['zip', 'tar']
+    },
+    {
+      name: 'path',
+      message: '请输入压缩文件路径(基于当前命令目录路径)',
+      type: 'input',
+      validate: (res) => {
+        if (!res) {
+          return false
+        }
+        return true
+      }
+    },
+    {
+      name: 'dir_name',
+      message: '请输入压缩文件名称',
+      type: 'input',
+      validate: (res) => {
+        if (!res) {
+          return false
+        }
+        return true
+      }
+    }
   ]
 }
 
